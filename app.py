f09dc599221b0ddf4aba7021410815496d2361c1
naoya25 <onaoya2002@gmail.com>
2023-09-20 10:28:08 +0900

    ready

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    image_path = '/static/images/cat.jpg'
+    name = 'cameron'
+    description = 'verry cute!'
+    btn_msg = 'more read'
+    return render_template('index.html', image_path=image_path, name=name, description=description, btn_msg=btn_msg)
 
 if __name__ == '__main__':
     app.run(port=5050)
